b8368d4c05a0a1f261403e898ba82b5b1e3b0181
Timur Kasimov <timurcat7@gmail.com>
2021-01-22 18:38:39 +0700

    Fixed init and repr. Added iter, reversed, delitem, eq and str methods. Removed setitem. Added type hints.

--- alds/linear/array/ord_array.py
+++ alds/linear/array/ord_array.py
@@ -1,12 +1,14 @@
+import numbers
+import reprlib
 from bisect import bisect, insort
-from typing import Any, List, Union, Sequence
+from typing import Any, List, Union, Sequence, Iterable, Optional, Iterator
 
 
 class OrdArray:
-    def __init__(self) -> None:
-        self.__data: List[Any] = []
+    def __init__(self, data: Optional[Iterable[Any]] = None) -> None:
+        self.__data: List[Any] = [] if data is None else sorted(list(data))
 
-    def find(self, value) -> int:
+    def find(self, value: Any) -> int:
         if len(self.__data) == 0:
             raise ValueError('trying to find value in empty array')
 
@@ -17,12 +19,12 @@ class OrdArray:
         else:
             raise ValueError('not found')
 
-    def insert(self, value) -> None:
+    def insert(self, value: Any) -> None:
         insort(self.__data, value)
 
-    def delete(self, value) -> None:
+    def delete(self, value: Any) -> None:
         try:
-            i = self.find(value)
+            i: int = self.find(value)
 
             del self.__data[i]
         except ValueError as err:
@@ -31,23 +33,35 @@ class OrdArray:
     def __len__(self) -> int:
         return len(self.__data)
 
-    def __getitem__(self, key: int) -> Union[Any, Sequence[Any]]:
-        if not isinstance(key, int):
-            raise TypeError(f'unsupported key type: {type(key)}, expected int instead')
+    def __iter__(self) -> Iterator[Any]:
+        return iter(self.__data)
 
+    def __reversed__(self) -> 'OrdArray':
+        cls = type(self)
+
+        return cls(self.__data[::-1])
+
+    def __getitem__(self, key: Union[numbers.Integral, slice]) -> Union[Any, 'OrdArray']:
+        cls = type(self)
+
+        if isinstance(key, numbers.Integral):
+            try:
+                return self.__data[key]
+            except IndexError as err:
+                raise IndexError from err
+        elif isinstance(key, slice):
+            return cls(self.__data[key])
+        else:
+            raise TypeError(f'unsupported key type: {type(key)}, expected numbers.Integral or slice instead')
+
+    def __delitem__(self, key: int) -> None:
         try:
-            return self.__data[key]
+            del self.__data[key]
         except IndexError as err:
             raise IndexError from err
 
-    def __setitem__(self, key: int, value: Any) -> None:
-        if not isinstance(key, int):
-            raise TypeError(f'unsupported key type: {type(key)}, expected int instead')
-
-        try:
-            self.__data[key] = value
-        except IndexError as err:
-            raise IndexError from err
+    def __eq__(self, other) -> bool:
+        return len(self.__data) == len(other) and all(a == b for a, b in zip(self.__data, other))
 
     def __contains__(self, item: Any) -> bool:
         try:
@@ -57,5 +71,10 @@ class OrdArray:
         except ValueError:
             return False
 
+    def __str__(self):
+        return str(self.__data)
+
     def __repr__(self) -> str:
-        return f'OrdArray([{", ".join([str(elem) for elem in self.__data])}])'
+        data = reprlib.repr(self.__data)
+
+        return f'OrdArray({data[data.find("["):-1]})'
